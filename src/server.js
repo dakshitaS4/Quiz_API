a24e1bf6191ddbaea8c5f06aee4a08b298b4d491
dakshitas4 <saxena.dakshita407@gmail.com>
2023-11-27 14:17:39 +0530

    update

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const routes = require('./routes') // includes the routes.js file
 const cors = require('cors') // includes cors module
 
-
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DB_URL_DEV).then(()=>{
     console.log("DATABASE CONNECTED!");
@@ -12,7 +12,7 @@ mongoose.connect(process.env.DB_URL_DEV).then(()=>{
     app.use(cors()) // We're telling express to use CORS
     app.use(express.json()) // we need to tell server to use json as well
     app.use("/",routes) // tells the server to use the routes in routes.js
-    app.listen(process.env.PORT, () => {
+    app.listen(PORT, () => {
         console.log("The API is running...")
     })
 })
